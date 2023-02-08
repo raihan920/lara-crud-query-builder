ca0b1c3932f24a9b0f7ee0ca72a69252bbe0737f
Raihan Chowdhury <raihan92@rocketmail.com>
2023-02-09 01:11:19 +0600

    some problem with seeding

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        DB::table('users')->insert([
+            'name' => $faker->name(),
+            'email' => $faker->email(),
+            'phone_number' => $faker->phoneNumber()
+        ]);
+    }
+}
